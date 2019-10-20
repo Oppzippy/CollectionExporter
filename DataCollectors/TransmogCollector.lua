d651b0e9c81ecf01437191037a49458ce46bdde6
Kyle Scheuing <oppzippy@gmail.com>
2019-10-20 18:18:11 -0400

    Add TODO

--- DataCollectors/TransmogCollector.lua
+++ DataCollectors/TransmogCollector.lua
@@ -2,7 +2,25 @@ local TransmogCollector = {}
 TransmogCollector.name = "transmog"
 
 function TransmogCollector:Collect()
-
+	local collection = {
+		illusions = {},
+		gear = {},
+	}
+	local illusions = collection.illusions
+	for _, illusion in ipairs(C_TransmogCollection.GetIllusions()) do
+		if illusion.isCollected then
+			local _, name = C_TransmogCollection.GetIllusionSourceInfo(illusion.sourceID)
+			illusions[#illusions+1] = {
+				visualID = illusion.visualID,
+				sourceID = illusion.sourceID,
+				name = name,
+			}
+		end
+	end
+	----------------------------------------------------------------------------
+	-- TODO: Gear
+	-- Set filters
+	-- Find list at WardrobeCollectionFrame.ItemsCollectionFrame.visualsList
 end
 
 CollectionExporter:RegisterDataCollector(TransmogCollector)
